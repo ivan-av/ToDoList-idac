51317d73be3d88bfc28626077ba64bbc894db764
Iván Avila <ivan.avicamp@gmail.com>
2022-07-26 12:09:37 -0500

    Defining routes

--- src/app.js
+++ src/app.js
@@ -3,10 +3,26 @@ const express = require("express")
 
 const app = express();
 
+const PORT = 8000;
+
 app.use(express())
 
-app.use("/todo", (req, res) => {
-    res.json({ message: "Use petition", method: req.method})
-})
+//?Defining routes and HTTP methods (verbs) of each route
+// !ROUTES                HTTP Method                   Description/Function
+//* /tasks              > GET (all tasks)               Obtain all the tasks
+//* /tasks/:id          > GET (task by id)              Obtain an specific task
+
+//* /tasks/             > for POST
+//* /tasks/:id          > for GET tasks by id
+
+
+//* app.use("/todo", (req, res) => {
+//     res.json({ message: "Use petition", method: req.method})
+// })
 
 // ? Here are all the routes of my tasks
+// app.use('/api/v1')
+
+app.listen(PORT, () => {
+    console.log("Server started at port 8000");
+});
---
+++ src/tasks/tasks.controllers.js
@@ -0,0 +1,11 @@
+const taskDB = [{
+    id: 1,
+    title: 'First to do',
+    description: 'Make an API CRUD about a ToDo manager',
+    isCompleted: false
+}]
+
+// ?Controller to return all my tasks
+const getAllTasks = () => {
+    return taskDB
+}
